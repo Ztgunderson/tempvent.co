04f9317726a84ccd800d61b0e6a590cb9f42111e
ztgunderson <ztgunderson@wisc.edu>
2025-05-27 17:34:30 -0500

    Newest Build v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 // app/layout.tsx
-import type { Metadata, Viewport } from 'next';
+import type { Metadata } from 'next';
 import { CartProvider } from '@/context';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import ClientLayoutWrapper from '../components/ClientLayoutWrapper';
 import './globals.css';
 
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 1,
-};
-
 export const metadata: Metadata = {
   title: 'Temp Vents: Temporary covers, cleaner than all the others',
   description: 'Professional vent protection solutions for home remodeling, construction, and more.',
+  viewport: 'width=device-width, initial-scale=1',
 };
 
 export default function RootLayout({
